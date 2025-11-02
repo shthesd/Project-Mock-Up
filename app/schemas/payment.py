b8959b598d444d61140ea23ff01497ea9f54fb19
shthesd <e1475050@u.nus.edu>
2025-11-03 00:30:59 +0800

    Update payment.py

--- app/schemas/payment.py
+++ app/schemas/payment.py
@@ -1,1 +1,10 @@
+from pydantic import BaseModel
+from datetime import date
+
+class PaymentCreate(BaseModel):
+    group_id: int
+    payer_id: int
+    payee_id: int
+    amount: float
+    date: date
 
