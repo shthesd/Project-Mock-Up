4b1d476748ce02fc3128faee954b9621f3a4138c
shthesd <e1475050@u.nus.edu>
2025-11-03 00:29:47 +0800

    Update auth.py

--- app/schemas/auth.py
+++ app/schemas/auth.py
@@ -1,1 +1,10 @@
+from pydantic import BaseModel, EmailStr
+
+class SignupIn(BaseModel):
+    email: EmailStr
+    password: str
+
+class TokenOut(BaseModel):
+    access_token: str
+    token_type: str = "bearer"
 
