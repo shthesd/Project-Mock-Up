9ef5823c6c9e98a8e04c24874cc82d307953b70e
shthesd <e1475050@u.nus.edu>
2025-11-03 00:26:01 +0800

    Update config.py

--- app/core/config.py
+++ app/core/config.py
@@ -1,1 +1,13 @@
+from pydantic_settings import BaseSettings
+
+class Settings(BaseSettings):
+    DATABASE_URL: str
+    SECRET_KEY: str
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 120
+    ENV: str = "development"
+
+    class Config:
+        env_file = ".env"
+
+settings = Settings()
 
